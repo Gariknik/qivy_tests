acec6b51af4f005f37233d844d9b0cfe581cf08e
Gariknik <Nikitenkivi@gmail.com>
2025-02-27 17:13:32 +0200

    second commit

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+import os
+os.environ['KIVY_TEXT'] = 'pil'
 
-class MainApp(MDApp):
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
+class LoginScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='User Name'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text="password"))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
+
+class MyApp(App):
     def build(self):
-        self.theme_cls.theme_style = "Dark"
-        return MDLabel(text="Hello, World", halign="center")
-    
+        return LoginScreen()
 
-if __name__ == "__main__":
-    MainApp().run()
+
+if __name__ == '__main__':
+    MyApp().run()
