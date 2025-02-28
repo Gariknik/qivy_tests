aa4fc4a260a75893552c9bc8d3852c41a3bcf4f5
Gariknik <Nikitenkivi@gmail.com>
2025-02-28 16:35:16 +0200

    working...

--- main.py
+++ main.py
@@ -2,25 +2,49 @@ import os
 os.environ['KIVY_TEXT'] = 'pil'
 
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.properties import ListProperty
 
-class LoginScreen(GridLayout):
+class RootWidget(AnchorLayout):
+
     def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text="password"))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
+        super(RootWidget, self).__init__(**kwargs)
+        self.anchor_x='right'
+        self.anchor_y='bottom'
+        self.add_widget(Button(text='btn 1', size_hint=(.3, .1)))
+        self.anchor_x='left'
+        self.anchor_y='bottom'
+        self.add_widget(Button(text='btn 2', size_hint=(.3, .1)))
+        # cb = CustomBtn()
+        # cb.bind(pressed=self.btn_pressed)
+        # self.add_widget(cb)
+        
 
-class MyApp(App):
+    def btn_pressed(self, instance, pos):
+        print('pos: printed from root widget: {pos}'.format(pos=pos))
+
+class CustomBtn(Widget):
+
+    pressed = ListProperty([0, 0])
+
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            self.pressed = touch.pos
+            # we consumed the touch. return False here to propagate
+            # the touch further to the children.
+            return True
+        return super(CustomBtn, self).on_touch_down(touch)
+
+    def on_pressed(self, instance, pos):
+        print('pressed at {pos}'.format(pos=pos))
+
+class TestApp(App):
+
     def build(self):
-        return LoginScreen()
+        return RootWidget()
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    TestApp().run()
